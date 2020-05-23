3eb27138c437a8b0339fae0a7ae415516217b148
sheth <sheth@harshil>
2020-05-23 13:06:10 +0530

    commit new pro

---
+++ Selenium-introduction/src/checkboxselect.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class checkboxselect {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","D:\\Udemy selenium classes\\chromedriver_82\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://book.spicejet.com/");
+		Thread.sleep(10000);
+		driver.findElement(By.cssSelector("input[id='ControlGroupSearchView_AvailabilitySearchInputSearchView_SeniorCitizen']")).click();
+		System.out.println(driver.findElement(By.cssSelector("input[id='ControlGroupSearchView_AvailabilitySearchInputSearchView_SeniorCitizen']")).isSelected());
+		System.out.println(driver.findElements(By.cssSelector("input[type='checkbox']")).size());
+	
+	}
+
+}
